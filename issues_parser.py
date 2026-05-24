2a197d82ced8a731541df53e7ce56b088a7d5dd5
skarbus67 <wroclawska148@gmail.com>
2026-05-24 16:39:28 +0200

    feat: add issue parser

---
+++ issues_parser.py
@@ -0,0 +1,29 @@
+from typing import Dict, List, Any
+
+
+def parse_github_issues(raw_issues: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+
+    parsed_issues = []
+    
+    for issue in raw_issues:
+        body_text = issue.get("body")
+        if not body_text:
+            body_text = ""
+
+            
+        labels = [label.get("name") for label in issue.get("labels")]
+        
+
+        parsed_issue = {
+            "id": issue.get("number"),
+            "title": issue.get("title"),
+            "body": body_text,
+            "state": issue.get("state"),
+            "created_at": issue.get("created_at"),
+            "author": issue.get("user").get("login"),
+            "labels": labels
+        }
+        
+        parsed_issues.append(parsed_issue)
+        
+    return parsed_issues
